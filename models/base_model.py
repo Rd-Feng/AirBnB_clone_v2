e16faf247034bac2c87f9b51e08998c1c466c7e3
Kevin Yook <yook00627@gmail.com>
2018-06-07 14:04:27 -0700

    Added args and kwargs for base model
    for the base model I edited the instanec method to have args and kwargs
    args is not used but when kwargs is passed in it will use the dictionary
    information to update the class

--- models/base_model.py
+++ models/base_model.py
@@ -9,16 +9,26 @@ class BaseModel:
     for other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Instantiation of base model class
+        Args:
+            args: it won't be used
+            kwargs: arguments for the constructor of the BaseModel
         Attributes:
             id: unique id generated
             created_at: creation date
             updated_at: updated date
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """returns a string
@@ -26,13 +36,13 @@ class BaseModel:
             returns a string of class name, id, and dictionary
         """
         return "[{}] ({}) {}".format(
-            str(type(self).__name__), self.id, str(self.__dict__))
+            type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """updates the public instance attribute updated_at to current
         """
         self.updated_at = datetime.now()
-
+ 
     def to_dict(self):
         """creates dictionary of the class  and returns
         Return:
