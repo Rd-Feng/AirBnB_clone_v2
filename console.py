adad38a9d957d2dbc4e9ec9eff52d2cb72d04fb9
Kevin Yook <yook00627@gmail.com>
2018-06-07 18:23:21 -0700

    create basic console
    created the basics of the console witch can exit the program and also made
    a custom prompt and made all the previouse codes pep8 validated

---
+++ console.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This is the console for AirBnB"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """this class is entry point of the command interpreter
+    """
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, line):
+        """Quit command to exit the program at end of file"""
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,7 @@
 import json
 from models.base_model import BaseModel
 
+
 class FileStorage:
     """This class serializes instances to a JSON file and
     deserializes JSON file to instances
