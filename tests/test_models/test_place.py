36cd1089bf74b5b4a1b0195715cd730d46e08923
Kevin Yook <yook00627@gmail.com>
2018-06-12 15:09:19 -0700

    update test case for place

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,90 +1,90 @@
 #!/usr/bin/python3
-
+"""test for place"""
 import unittest
 import os
-import pep8
 from models.place import Place
 from models.base_model import BaseModel
+import pep8
 
 
 class TestPlace(unittest.TestCase):
+    """this will test the place class"""
 
     @classmethod
     def setUpClass(cls):
-        cls.place1 = Place()
-        cls.place1.city_id = "Somewhere in India"
-        cls.place1.user_id = "Aladdin"
-        cls.place1.name = "Taj Mahal"
-        cls.place1.description = "Fit for a king"
-        cls.place1.number_rooms = 0
-        cls.place1.number_bathrooms = 0
-        cls.place1.max_guest = 0
-        cls.place1.price_by_night = 0
-        cls.place1.latitude = 0.0
-        cls.place1.longitude = 0.0
-        cls.place1.amenity_ids = []
+        """set up for test"""
+        cls.place = Place()
+        cls.place.city_id = "1234-abcd"
+        cls.place.user_id = "4321-dcba"
+        cls.place.name = "Death Star"
+        cls.place.description = "UNLIMITED POWER!!!!!"
+        cls.place.number_rooms = 1000000
+        cls.place.number_bathrooms = 1
+        cls.place.max_guest = 607360
+        cls.place.price_by_night = 10
+        cls.place.latitude = 160.0
+        cls.place.longitude = 120.0
+        cls.place.amenity_ids = ["1324-lksdjkl"]
 
     @classmethod
     def teardown(cls):
-        del cls.place1
+        """at the end of the test this will tear it down"""
+        del cls.place
 
     def tearDown(self):
+        """teardown"""
         try:
             os.remove("file.json")
         except:
             pass
 
-    def test_style_check(self):
-        """
-        Tests pep8 style
-        """
+    def test_pep8_Place(self):
+        """Tests pep8 style"""
         style = pep8.StyleGuide(quiet=True)
         p = style.check_files(['models/place.py'])
         self.assertEqual(p.total_errors, 0, "fix pep8")
 
-    def test_is_subclass(self):
-        self.assertTrue(issubclass(self.place1.__class__, BaseModel), True)
-
-    def test_checking_for_functions(self):
+    def test_checking_for_docstring_Place(self):
         self.assertIsNotNone(Place.__doc__)
 
-    def test_has_attributes(self):
-        self.assertTrue('id' in self.place1.__dict__)
-        self.assertTrue('created_at' in self.place1.__dict__)
-        self.assertTrue('updated_at' in self.place1.__dict__)
-        self.assertTrue('city_id' in self.place1.__dict__)
-        self.assertTrue('user_id' in self.place1.__dict__)
-        self.assertTrue('name' in self.place1.__dict__)
-        self.assertTrue('description' in self.place1.__dict__)
-        self.assertTrue('number_rooms' in self.place1.__dict__)
-        self.assertTrue('number_bathrooms' in self.place1.__dict__)
-        self.assertTrue('max_guest' in self.place1.__dict__)
-        self.assertTrue('price_by_night' in self.place1.__dict__)
-        self.assertTrue('latitude' in self.place1.__dict__)
-        self.assertTrue('longitude' in self.place1.__dict__)
-        self.assertTrue('amenity_ids' in self.place1.__dict__)
+    def test_has_attributes_Place(self):
+        self.assertTrue('id' in self.place.__dict__)
+        self.assertTrue('created_at' in self.place.__dict__)
+        self.assertTrue('updated_at' in self.place.__dict__)
+        self.assertTrue('city_id' in self.place.__dict__)
+        self.assertTrue('user_id' in self.place.__dict__)
+        self.assertTrue('name' in self.place.__dict__)
+        self.assertTrue('description' in self.place.__dict__)
+        self.assertTrue('number_rooms' in self.place.__dict__)
+        self.assertTrue('number_bathrooms' in self.place.__dict__)
+        self.assertTrue('max_guest' in self.place.__dict__)
+        self.assertTrue('price_by_night' in self.place.__dict__)
+        self.assertTrue('latitude' in self.place.__dict__)
+        self.assertTrue('longitude' in self.place.__dict__)
+        self.assertTrue('amenity_ids' in self.place.__dict__)
 
+    def test_is_subclass_Place(self):
+        self.assertTrue(issubclass(self.place.__class__, BaseModel), True)
 
-    def test_attributes_are_strings(self):
-        self.assertEqual(type(self.place1.city_id), str)
-        self.assertEqual(type(self.place1.user_id), str)
-        self.assertEqual(type(self.place1.name), str)
-        self.assertEqual(type(self.place1.description), str)
-        self.assertEqual(type(self.place1.number_rooms), int)
-        self.assertEqual(type(self.place1.number_bathrooms), int)
-        self.assertEqual(type(self.place1.max_guest), int)
-        self.assertEqual(type(self.place1.price_by_night), int)
-        self.assertEqual(type(self.place1.latitude), float)
-        self.assertEqual(type(self.place1.longitude), float)
-        self.assertEqual(type(self.place1.amenity_ids), list)
+    def test_attribut_types_Place(self):
+        self.assertEqual(type(self.place.city_id), str)
+        self.assertEqual(type(self.place.user_id), str)
+        self.assertEqual(type(self.place.name), str)
+        self.assertEqual(type(self.place.description), str)
+        self.assertEqual(type(self.place.number_rooms), int)
+        self.assertEqual(type(self.place.number_bathrooms), int)
+        self.assertEqual(type(self.place.max_guest), int)
+        self.assertEqual(type(self.place.price_by_night), int)
+        self.assertEqual(type(self.place.latitude), float)
+        self.assertEqual(type(self.place.longitude), float)
+        self.assertEqual(type(self.place.amenity_ids), list)
 
+    def test_save_Place(self):
+        self.place.save()
+        self.assertNotEqual(self.place.created_at, self.place.updated_at)
 
-    def test_save(self):
-        self.place1.save()
-        self.assertNotEqual(self.place1.created_at, self.place1.updated_at)
-
-    def test_to_dict(self):
-        self.assertEqual('to_dict' in dir(self.place1), True)
+    def test_to_dict_Place(self):
+        self.assertEqual('to_dict' in dir(self.place), True)
 
 if __name__ == "__main__":
     unittest.main()
