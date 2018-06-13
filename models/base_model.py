cb398262ca04092269f5312bef7cb8f7547bf9d0
Kevin Yook <yook00627@gmail.com>
2018-06-13 10:09:05 -0700

    made time equal to each other

--- models/base_model.py
+++ models/base_model.py
@@ -28,8 +28,7 @@ class BaseModel:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
