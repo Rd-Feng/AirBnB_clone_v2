2b9b4a6aa82f858f99faca55b3b7b62072f8c216
Rd-Feng <394@holbertonschool.com>
2019-01-08 11:16:38 -0800

    Feature: cities getter

--- models/state.py
+++ models/state.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import models
 
 
 class State(BaseModel, Base):
@@ -13,3 +14,13 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade="all, delete", backref="state")
+
+    @property
+    def cities(self):
+        """get all cities with the current state id
+        from filestorage
+        """
+        l = [v for k, v in models.storage.all(models.City).items()
+            if v.state_id == self.id
+        ]
+        return (l)
