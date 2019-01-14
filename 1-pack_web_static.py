f2ede15eb4453d3b7f18a051981e881cbb3249c0
Rd-Feng <394@holbertonschool.com>
2019-01-14 15:10:37 -0800

    put archives under versions

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,14 +3,18 @@
 """
 from fabric.api import local
 from datetime import datetime
+import os
 
 
 def do_pack():
     """pack all content within web_static
     into a .tgz archive
+    The archive will be put in versions/
     """
+    if not os.path.exists("versions"):
+        local("mkdir versions")
     now = datetime.now()
-    name = "web_static_{}.tgz".format(
+    name = "versions/web_static_{}.tgz".format(
         now.strftime("%Y%m%d%H%M%S")
     )
     cmd = "tar -cvzf {} {}".format(name, "web_static")
