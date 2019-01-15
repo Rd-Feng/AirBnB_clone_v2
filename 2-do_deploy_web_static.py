b64bbc81155ca6709df58f6ab15829926c767aee
Rd-Feng <394@holbertonschool.com>
2019-01-14 16:05:14 -0800

    init commit

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""pack and deploy content to server
+"""
+from fabric.api import local, env, run, put
+from datetime import datetime
+import os
+env.hosts = ['35.231.156.161', '34.73.64.44']
+env.user = 'ubuntu'
+
+
+def do_pack():
+    """pack all content within web_static
+    into a .tgz archive
+    The archive will be put in versions/
+    """
+    if not os.path.exists("versions"):
+        local("mkdir versions")
+    now = datetime.now()
+    name = "versions/web_static_{}.tgz".format(
+        now.strftime("%Y%m%d%H%M%S")
+    )
+    cmd = "tar -cvzf {} {}".format(name, "web_static")
+    result = local(cmd)
+    if not result.failed:
+        return name
+
+
+def do_deploy(archive_path):
+    """deploy package to remote server
+    Arguments:
+        archive_path: path to archive to deploy
+    """
+    if not archive_path or not os.path.exists(archive_path):
+        return False
+    put(archive_path, '/tmp')
+    ar_name = archive_path[archive_path.find("/") + 1: -4]
+    try:
+        run('mkdir -p /data/web_static/releases/{}/'.format(ar_name))
+        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'.format(
+                ar_name, ar_name
+        ))
+        run('rm /tmp/{}.tgz'.format(ar_name))
+        run('mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/'.format(
+                ar_name, ar_name
+        ))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(
+            ar_name
+        ))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(
+            ar_name
+        ))
+        return True
+    except:
+        return False
