3d11ebc4e8e32ee94704abaeab350735cf6b5b6d
Rd-Feng <394@holbertonschool.com>
2019-01-29 13:02:54 -0800

    init commit

---
+++ web_flask/9-states.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""simple flask app
+"""
+from flask import Flask, render_template
+from models import storage
+from os import environ as env
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    """reload storage after each request
+    """
+    storage.close()
+
+
+@app.route("/states/<id>")
+@app.route("/states")
+def states_cities_list(id=None):
+    """show state and cities if id is given
+    otherwise list all states
+    """
+    from models.state import State
+    states = (
+        storage.all("State")
+        if env.get('HBNB_TYPE_STORAGE') == 'db'
+        else storage.all(State)
+    )
+    if id:
+        state = states.get('State.{}'.format(id))
+        states = [state] if state else []
+    else:
+        states = list(states.values())
+    states.sort(key=lambda x: x.name)
+    for state in states:
+        state.cities.sort(key=lambda x: x.name)
+    return render_template(
+        '9-states.html',
+        states=states,
+        len=len(states),
+        id=id
+    )
+
+
+app.run(host='0.0.0.0', port=5000)
