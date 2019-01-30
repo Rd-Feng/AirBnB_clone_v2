43acaef5770fa720c0884d2d785cc053440a2e5c
Rd-Feng <394@holbertonschool.com>
2019-01-30 13:17:54 -0800

    init commit

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """hbnb filter
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, Markup
 from models import storage
+import sys
 app = Flask(__name__)
 
 
@@ -25,6 +26,9 @@ def states_cities_list():
     amenities = list(storage.all("Amenity").values())
     amenities.sort(key=lambda x: x.name)
     places = list(storage.all("Place").values())
+    places.sort(key=lambda x: x.name)
+    for place in places:
+        place.description = Markup(place.description)
     return render_template(
         '100-hbnb.html',
         states=states,
